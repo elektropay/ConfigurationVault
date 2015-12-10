12de282f0f4582cf006316088c7b5eb470daf31d
UCSDMath <ucsdmath.project@gmail.com>
2015-12-10 10:30:28 -0800

    Update 1.5.x-dev

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# ConfigurationVault[![Latest Stable Version](https://poser.pugx.org/ucsdmath/configuration-vault/v/stable)](https://packagist.org/packages/ucsdmath/configuration-vault)[![License](https://poser.pugx.org/ucsdmath/configuration-vault/license)](https://packagist.org/packages/ucsdmath/configuration-vault)[![Total Downloads](https://poser.pugx.org/ucsdmath/configuration-vault/downloads)](https://packagist.org/packages/ucsdmath/configuration-vault)[![Latest Unstable Version](https://poser.pugx.org/ucsdmath/configuration-vault/v/unstable)](https://packagist.org/packages/ucsdmath/configuration-vault)[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/ucsdmath/ConfigurationVault/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/ucsdmath/ConfigurationVault/?branch=master)[![Build Status](https://scrutinizer-ci.com/g/ucsdmath/ConfigurationVault/badges/build.png?b=master)](https://scrutinizer-ci.com/g/ucsdmath/ConfigurationVault/build-status/master)ConfigurationVault is a testing and development library only. This is not to be used in a production.## Installation using [Composer](http://getcomposer.org/)You can install the class ```ConfigurationVault``` with Composer and Packagist byadding the ucsdmath/configuration-vault package to your composer.json file:```"require": {    "php": ">=5.6",    "ucsdmath/configuration-vault": "dev-master"},```Or you can add the class directly from the terminal prompt:```bash$ composer require ucsdmath/configuration-vault```## Usage``` php$vault = new \UCSDMath\ConfigurationVault\ConfigurationVault();```## DocumentationNo documentation site available at this time.<!-- [Check out the documentation](http://math.ucsd.edu/~deisner/documentation/ConfigurationVault/) -->## Testing``` bash$ phpunit```## ContributingPlease see [CONTRIBUTING](CONTRIBUTING.md) for details.## SecurityIf you discover any security related issues, please email deisner@ucsd.edu instead of using the issue tracker.## Credits- [Daryl Eisner](https://github.com/UCSDMath)- [All Contributors](../../contributors)## LicenseThe MIT License (MIT). Please see [License File](LICENSE) for more information.
+# ConfigurationVault
+
+[![Latest Stable Version](https://poser.pugx.org/ucsdmath/configuration-vault/v/stable)](https://packagist.org/packages/ucsdmath/configuration-vault)
+[![License](https://poser.pugx.org/ucsdmath/configuration-vault/license)](https://packagist.org/packages/ucsdmath/configuration-vault)
+[![Total Downloads](https://poser.pugx.org/ucsdmath/configuration-vault/downloads)](https://packagist.org/packages/ucsdmath/configuration-vault)
+[![Latest Unstable Version](https://poser.pugx.org/ucsdmath/configuration-vault/v/unstable)](https://packagist.org/packages/ucsdmath/configuration-vault)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/ucsdmath/ConfigurationVault/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/ucsdmath/ConfigurationVault/?branch=master)
+[![Build Status](https://scrutinizer-ci.com/g/ucsdmath/ConfigurationVault/badges/build.png?b=master)](https://scrutinizer-ci.com/g/ucsdmath/ConfigurationVault/build-status/master)
+
+ConfigurationVault is a testing and development library only. This is not to be used in a production.
+
+## Installation using [Composer](http://getcomposer.org/)
+You can install the class ```ConfigurationVault``` with Composer and Packagist by
+adding the ucsdmath/configuration-vault package to your composer.json file:
+
+```
+"require": {
+    "php": ">=5.6",
+    "ucsdmath/configuration-vault": "dev-master"
+},
+```
+Or you can add the class directly from the terminal prompt:
+
+```bash
+$ composer require ucsdmath/configuration-vault
+```
+
+## Usage
+
+``` php
+$vault = new \UCSDMath\ConfigurationVault\ConfigurationVault();
+```
+
+## Documentation
+
+No documentation site available at this time.
+<!-- [Check out the documentation](http://math.ucsd.edu/~deisner/documentation/ConfigurationVault/) -->
+
+## Testing
+
+``` bash
+$ phpunit
+```
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+## Security
+
+If you discover any security related issues, please email deisner@ucsd.edu instead of using the issue tracker.
+
+## Credits
+
+- [Daryl Eisner](https://github.com/UCSDMath)
+- [All Contributors](../../contributors)
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE) for more information.
--- src/ConfigurationVault/ConfigurationVault.php
+++ src/ConfigurationVault/ConfigurationVault.php
@@ -1,1 +1,137 @@
-<?php/* * This file is part of the UCSDMath package. * * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu> * * For the full copyright and license information, please view the LICENSE * file that was distributed with this source code. */namespace UCSDMath\Configuration\ConfigurationVault;use UCSDMath\Filesystem\FilesystemInterface;use UCSDMath\Serialization\Yaml\YamlInterface;/** * ConfigurationVault is the default implementation of {@link ConfigurationVaultInterface} which * provides routine configuration-vault methods that are commonly used throughout the framework. * * Method list: * * @method ConfigurationVaultInterface __construct(FilesystemInterface $filesystem, YamlInterface $yaml); * @method array getResultDataSet(); * @method Boolean isVaultFileReadable(); * @method ConfigurationVaultInterface reset(); * @method ConfigurationVaultInterface setVaultFileDefaultEnvironment($value); * * @author Daryl Eisner <deisner@ucsd.edu> * * @api */class ConfigurationVault extends AbstractConfigurationVault implements ConfigurationVaultInterface{    /**     * Constants.     *     * @var string VERSION  A version number     *     * @api     */    const VERSION = '1.5.0';    // --------------------------------------------------------------------------    /**     * Properties.     */    // --------------------------------------------------------------------------    /**     * Constructor.     *     * @param FilesystemInterface  $filesystem A FilesystemInterface Interface instance     * @param YamlInterface        $yaml       A YamlInterface Interface instance     *     * @api     */    public function __construct(        FilesystemInterface $filesystem,        YamlInterface $yaml    ) {        parent::__construct($filesystem, $yaml);    }    // --------------------------------------------------------------------------    /**     * Pull the entire dataset.     *     * @return array     */    public function getResultDataSet()    {        return $this->getProperty('resultDataSet');    }    // --------------------------------------------------------------------------    /**     * Does file exist and is readable.     *     * @return bool     */    public function isVaultFileReadable()    {        return is_readable($this->vaultSettingsDirectory . '/' . $this->vaultFilename);    }    // --------------------------------------------------------------------------    /**     * Set the default environment (e.g., 'development', 'staging', 'production').     *     * @param  string $value  A default environment type     *     * @return ConfigurationVaultInterface     *     * @api     */    public function setVaultFileDefaultEnvironment($value)    {        $this->setProperty('vaultFileDefaultEnvironment', strtolower(trim($value)));        return $this;    }    // --------------------------------------------------------------------------    /**     * Reset to default settings.     *     * @return ConfigurationVaultInterface     *     * @api     */    public function reset()    {        $this->setProperty('cipherKey', null);        $this->setProperty('vaultFilename', null);        $this->setProperty('vaultFileType', null);        $this->setProperty('vaultRecordId', null);        $this->setProperty('vaultRecordUUID', null);        $this->setProperty('vaultRecordDate', null);        $this->setProperty('resultDataSet', array());        $this->setProperty('storageRegister', array());        $this->setProperty('vaultRecordEncrypted', null);        $this->setProperty('vaultFileDefaultSection', null);        $this->setProperty('vaultFileEnvironments', array());        $this->setProperty('vaultFileRequestedSection', null);        $this->setProperty('vaultFileDefaultEnvironment', null);        $this->setProperty('theAccountRootPath', realpath(__DIR__ . '/../../../../../../../../../../'));        return $this;    }}
+<?php
+
+/*
+ * This file is part of the UCSDMath package.
+ *
+ * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace UCSDMath\Configuration\ConfigurationVault;
+
+use UCSDMath\Filesystem\FilesystemInterface;
+use UCSDMath\Serialization\Yaml\YamlInterface;
+
+/**
+ * ConfigurationVault is the default implementation of {@link ConfigurationVaultInterface} which
+ * provides routine configuration-vault methods that are commonly used throughout the framework.
+ *
+ * Method list:
+ *
+ * @method ConfigurationVaultInterface __construct(FilesystemInterface $filesystem, YamlInterface $yaml);
+ * @method array getResultDataSet();
+ * @method Boolean isVaultFileReadable();
+ * @method ConfigurationVaultInterface reset();
+ * @method ConfigurationVaultInterface setVaultFileDefaultEnvironment($value);
+ *
+ * @author Daryl Eisner <deisner@ucsd.edu>
+ *
+ * @api
+ */
+class ConfigurationVault extends AbstractConfigurationVault implements ConfigurationVaultInterface
+{
+    /**
+     * Constants.
+     *
+     * @var string VERSION  A version number
+     *
+     * @api
+     */
+    const VERSION = '1.5.0';
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Properties.
+     */
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Constructor.
+     *
+     * @param FilesystemInterface  $filesystem A FilesystemInterface Interface instance
+     * @param YamlInterface        $yaml       A YamlInterface Interface instance
+     *
+     * @api
+     */
+    public function __construct(
+        FilesystemInterface $filesystem,
+        YamlInterface $yaml
+    ) {
+        parent::__construct($filesystem, $yaml);
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Pull the entire dataset.
+     *
+     * @return array
+     */
+    public function getResultDataSet()
+    {
+        return $this->getProperty('resultDataSet');
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Does file exist and is readable.
+     *
+     * @return bool
+     */
+    public function isVaultFileReadable()
+    {
+        return is_readable($this->vaultSettingsDirectory . '/' . $this->vaultFilename);
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Set the default environment (e.g., 'development', 'staging', 'production').
+     *
+     * @param  string $value  A default environment type
+     *
+     * @return ConfigurationVaultInterface
+     *
+     * @api
+     */
+    public function setVaultFileDefaultEnvironment($value)
+    {
+        $this->setProperty('vaultFileDefaultEnvironment', strtolower(trim($value)));
+
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Reset to default settings.
+     *
+     * @return ConfigurationVaultInterface
+     *
+     * @api
+     */
+    public function reset()
+    {
+        $this->setProperty('cipherKey', null);
+        $this->setProperty('vaultFilename', null);
+        $this->setProperty('vaultFileType', null);
+        $this->setProperty('vaultRecordId', null);
+        $this->setProperty('vaultRecordUUID', null);
+        $this->setProperty('vaultRecordDate', null);
+        $this->setProperty('resultDataSet', array());
+        $this->setProperty('storageRegister', array());
+        $this->setProperty('vaultRecordEncrypted', null);
+        $this->setProperty('vaultFileDefaultSection', null);
+        $this->setProperty('vaultFileEnvironments', array());
+        $this->setProperty('vaultFileRequestedSection', null);
+        $this->setProperty('vaultFileDefaultEnvironment', null);
+        $this->setProperty('theAccountRootPath', realpath(__DIR__ . '/../../../../../../../../../../'));
+
+        return $this;
+    }
+}
