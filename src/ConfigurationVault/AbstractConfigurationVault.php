741751d8399a5899cc25d8f6fcb5923bb9a05fdb
UCSDMath <UCSDMath.Project@gmail.com>
2017-05-20 07:25:33 -0700

    Update

--- src/ConfigurationVault/AbstractConfigurationVault.php
+++ src/ConfigurationVault/AbstractConfigurationVault.php
@@ -512,13 +512,13 @@ abstract class AbstractConfigurationVault implements
             throw new IOException(sprintf('The directory path %s does not exist. Check parameter: %s.', $directoryPath, __METHOD__), 0, null, $directoryPath);
         }
 
-        [$directoryPath, $rootPath] = [$directoryPath, sprintf('%s/../%s', $_SERVER['DOCUMENT_ROOT'], static::VAULT_DIRECTORY_NAME)];
+        [$directoryPath, $rootPath] = [realpath((string)$directoryPath), realpath(sprintf('%s/../%s', $_SERVER['DOCUMENT_ROOT'], static::VAULT_DIRECTORY_NAME))];
 
         return $this->setProperty(
             'vaultSettingsDirectory',
-            $directoryPath === null
-                ? (realpath($rootPath) ? realpath($rootPath) : null)
-                : (realpath($directoryPath) ? realpath($directoryPath) : null)
+            $directoryPath === true
+                ? $directoryPath
+                : ($rootPath ? $rootPath : null)
         );
     }
 
