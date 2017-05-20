3bdb2eb877013e059c94f316d27ebc3199a4e8a2
UCSDMath <UCSDMath.Project@gmail.com>
2017-05-20 06:27:17 -0700

    Update

--- src/ConfigurationVault/AbstractConfigurationVault.php
+++ src/ConfigurationVault/AbstractConfigurationVault.php
@@ -506,12 +506,11 @@ abstract class AbstractConfigurationVault implements
     public function setVaultSettingsDirectory(string $directoryPath = null): ConfigurationVaultInterface
     {
         $rootPath = null;
-
         if ($directoryPath !== null && !is_dir($directoryPath)) {
             throw new IOException(sprintf('The directory path %s does not exist. Check parameter: %s.', $directoryPath, __METHOD__), 0, null, $directoryPath);
         }
 
-        [$directoryPath, $rootPath] = [realpath((string) $directoryPath), realpath(sprintf('%s/../%s', $_SERVER['DOCUMENT_ROOT'], static::VAULT_DIRECTORY_NAME)];
+        [$directoryPath, $rootPath] = [realpath((string) $directoryPath), realpath(sprintf('%s/../%s', $_SERVER['DOCUMENT_ROOT'], static::VAULT_DIRECTORY_NAME))];
 
         return $this->setProperty('vaultSettingsDirectory', $directoryPath === false ? ($rootPath ? $rootPath : null) : ($directoryPath ? $directoryPath : null));
     }
