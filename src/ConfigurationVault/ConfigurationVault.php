b44cb702a03c5a355b563c8d2ecdc1d45e1c85c1
UCSDMath <ucsdmath.project@gmail.com>
2017-08-09 10:46:23 -0700

    Update

--- src/ConfigurationVault/ConfigurationVault.php
+++ src/ConfigurationVault/ConfigurationVault.php
@@ -53,7 +53,7 @@ class ConfigurationVault extends AbstractConfigurationVault implements Configura
      *
      * @api
      */
-    public const VERSION = '1.23.0';
+    public const VERSION = '1.24.0';
 
     //--------------------------------------------------------------------------
 
